e8a23f48b27eedb6f0fb868d4dd78dac591c0501
ronaldo-avalos <ronaldoadan1@gmail.com>
2025-03-10 12:13:16 -0600

    deploy wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/my-portfolio",
+  images: {
+    unoptimized: true, // GitHub Pages no soporta la optimización de imágenes de Next.js
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
